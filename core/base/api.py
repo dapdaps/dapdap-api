f8f451fda434902529a9bb48a626c3c50dbcdfd0
liuhanyu <429238123@qq.com>
2023-10-17 22:38:13 +0800

    add test api

--- core/base/api.py
+++ core/base/api.py
@@ -5,7 +5,7 @@
 import logging
 from fastapi import APIRouter
 from starlette.requests import Request
-
+import requests
 from core.utils.base_util import get_limiter
 
 logger = logging.getLogger(__name__)
@@ -22,3 +22,29 @@ router = APIRouter()
 async def health_check(request: Request):
     logger.info(request)
     return {"message": "Running!"}
+
+
+@router.get('/get_quote', tags=['base quote'])
+@limiter.limit('5/second')
+async def get_quote(request: Request):
+    res = requests.post("https://api.uniswap.org/v2/quote", data={
+        "tokenInChainId": 1,
+        "tokenIn": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        "tokenOutChainId": 1,
+        "tokenOut": "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        "amount": "995000000000000000",
+        "sendPortionEnabled": False,
+        "type": "EXACT_INPUT",
+        "configs": [
+            {
+                "protocols": [
+                    "V3"
+                ],
+                "enableUniversalRouter": False,
+                "routingType": "CLASSIC",
+                "recipient": "0xc25d79fc4970479b88068ce8891ed9be5799210d"
+            }
+        ]
+    })
+    res = res.json()
+    return res
