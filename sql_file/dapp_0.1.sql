af737e35b6962a5524e513fd9121172ac1cf7078
jaki <jaki@jaki.me>
2023-11-23 08:45:28 +0800

    dapp sql

--- sql_file/dapp_0.1.sql
+++ sql_file/dapp_0.1.sql
@@ -1,25 +1,71 @@
-CREATE TABLE "chain" (
+CREATE TABLE "network" (
     "id" BIGSERIAL NOT NULL PRIMARY KEY,
+    "chain_id" INT NOT NULL,
+    "name" VARCHAR(128) NOT NULL,
+    "technology" VARCHAR(100) NULL,
+    "description" VARCHAR(1000) NULL,
+    "native_token" VARCHAR(50) NULL,
+    "milestones" TEXT NULL,
     "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-    "network_id" VARCHAR(128) NOT NULL UNIQUE,
-    "technology" VARCHAR(100) NOT NULL,
-    "description" VARCHAR(1000),
-    "native_token" VARCHAR(50),
-    "milestones" TEXT
+    "updated_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
 );
+
 
 CREATE TABLE "dapp" (
     "id" BIGSERIAL NOT NULL PRIMARY KEY,
+    "name" VARCHAR(128) NOT NULL,
+    "description" VARCHAR(1000) NULL,
+    "native_token" VARCHAR(50) NULL,
+    "favorite" INT NULL DEFAULT 0,
+    "recommend" BOOL NULL DEFAULT False,
+    "recommend_icon" VARCHAR(100) NULL,
     "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
-    "template" VARCHAR(255) NOT NULL,
-    "description" VARCHAR(1000),
-    "favorite" INT NOT NULL,
-    "native_token" VARCHAR(50),
-    "quest" BOOL   DEFAULT False,
-    "chains" VARCHAR(300) NOT NULL,
-    "functions" VARCHAR(200) NOT NULL,
-    "show" BOOL NOT NULL  DEFAULT False
+    "updated_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
 );
-CREATE INDEX "idx_dapp_templat" ON "dapp" ("template");
+
+
+CREATE TABLE "category" (
+    "id" BIGSERIAL NOT NULL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL UNIQUE,
+    "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
+);
+COMMENT ON COLUMN "category"."name" IS 'Bridge,Dex,Lending,Liquidity,Staking,Yield';
+
+
+CREATE TABLE "dapp_favorite" (
+    "id" BIGSERIAL NOT NULL PRIMARY KEY,
+    "account_id" INT NOT NULL,
+    "dapp_id" INT NOT NULL,
+    "is_favorite" BOOL NOT NULL DEFAULT False,
+    "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
+);
+CREATE UNIQUE INDEX "idx_dapp_favorite_account_id" ON "dapp_favorite" ("account_id","dapp_id");
+
+
+CREATE TABLE "dapp_relate" (
+    "id" BIGSERIAL NOT NULL PRIMARY KEY,
+    "dapp_id" INT NOT NULL,
+    "dapp_id_relate" INT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX "idx_dapp_relate_dapp_id" ON "dapp_relate" ("dapp_id");
+
+
+CREATE TABLE "dapp_network" (
+    "id" BIGSERIAL NOT NULL PRIMARY KEY,
+    "dapp_id" INT NOT NULL,
+    "network_id" INT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX "idx_dapp_network_network_id" ON "dapp_network" ("network_id");
+CREATE INDEX "idx_dapp_network_dapp_id" ON "dapp_network" ("dapp_id");
+
+
+CREATE TABLE "dapp_category" (
+    "id" BIGSERIAL NOT NULL PRIMARY KEY,
+    "dapp_id" INT NOT NULL,
+    "category_id" INT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX "idx_dapp_category_category_id" ON "dapp_category" ("category_id");
+CREATE UNIQUE INDEX "idx_dapp_category_dapp_id" ON "dapp_category" ("dapp_id","category_id");
