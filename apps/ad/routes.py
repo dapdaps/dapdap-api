f35ad908abac37fa91be99442f0a9bb558e9916a
jaki <jaki@jaki.me>
2023-12-19 18:49:36 +0800

    ad

--- apps/ad/routes.py
+++ apps/ad/routes.py
@@ -12,10 +12,13 @@ limiter = get_limiter()
 router = APIRouter(prefix="/api/ad")
 
 
-@router.get('', tags=['network'])
-@limiter.limit('60/minute', )
-async def get_one(request: Request, category: str, category_id: int):
-    ad = await Ad.filter(category=category, category_id=category_id).first()
+@router.get('', tags=['ad'])
+@limiter.limit('60/minute')
+async def get_one(request: Request, category: str, category_id: int = 0):
+    if category_id > 0:
+        ad = await Ad.filter(category=category, category_id=category_id).all()
+    else:
+        ad = await Ad.filter(category=category).order_by('-updated_at').all()
     return success(ad)
 
 
